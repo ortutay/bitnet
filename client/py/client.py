87787932f7725fcbb1517f14aaf0f56626b7d8fe
ortutay <marcell.ortutay@gmail.com>
2014-08-09 23:54:56 -0700

    rm junk

--- client/py/client.py
+++ client/py/client.py
@@ -3,9 +3,7 @@
 import websocket
 
 if __name__ == "__main__":
-  print "hello"
   addr = "localhost:4000"
   ws = websocket.create_connection("ws://" + addr + "/")
   ws.send("hello")
-  print "sent"
   ws.close()
