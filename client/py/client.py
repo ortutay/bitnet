f499935d35be379dcfc050b4e8a5fe9fbe08b74e
ortutay <marcell.ortutay@gmail.com>
2014-08-10 23:36:39 -0700

    rm py client for now

--- client/py/client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import websocket
-
-if __name__ == "__main__":
-  addr = "localhost:4000"
-  ws = websocket.create_connection("ws://" + addr + "/")
-  ws.send("hello")
-  ws.close()
