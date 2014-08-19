5554ef4c1b30465aa671f028d9435fb0e9bd2056
ortutay <marcell.ortutay@gmail.com>
2014-08-18 23:09:59 -0700

    button and text window

--- wallets/electrum/plugins/bitnet.py
+++ wallets/electrum/plugins/bitnet.py
@@ -1,4 +1,10 @@
 from electrum import BasePlugin
+from electrum.i18n import _
+
+import PyQt4
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import PyQt4.QtCore as QtCore
 
 class Plugin(BasePlugin):
     def fullname(self): return 'Bitnet'
@@ -6,6 +12,7 @@ class Plugin(BasePlugin):
     def description(self): return 'Communication network for Bitcoin wallets'
 
     def __init__(self, gui, name):
+        self.gui = gui
         BasePlugin.__init__(self, gui, name)
         self._is_available = self._init()
 
@@ -17,3 +24,14 @@ class Plugin(BasePlugin):
 
     def enable(self):
         return BasePlugin.enable(self)
+
+    def init(self):
+        self.gui.main_window.tabs.addTab(self.create_bitnet_tab(), _('Bitnet'))
+    
+    def create_bitnet_tab(self):
+        w = QWidget()
+        self.bitnet_grid = grid = QGridLayout(w)
+        grid.setSpacing(8)
+        grid.addWidget(QPushButton("Get tokens", w), 0, 0)
+        grid.addWidget(QPlainTextEdit("Hello", w), 0, 1)
+        return w
