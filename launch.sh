f876b9ba4c1d6c5845179a6508c7b96f3688421f
ortutay <marcell.ortutay@gmail.com>
2014-08-23 23:14:01 -0700

    launch script for AWS

---
+++ launch.sh
@@ -0,0 +1,16 @@
+set -euo pipefail
+IFS=$'\n\t'
+
+IP=$1
+FLAGS=$2
+
+BINARY_NAME=bitnet_server
+
+go build -o $BINARY_NAME bitbucket.org/ortutay/bitnet/server
+
+echo "Connecting to $IP to launch $BINARY_NAME with flags $FLAGS"
+
+ssh -i ~/keypair.pem "ubuntu@$IP" "if pgrep $BINARY_NAME &> /dev/null ; then killall $BINARY_NAME ; fi"
+scp -i ~/keypair.pem "./$BINARY_NAME" "ubuntu@$IP:/home/ubuntu/$BINARY_NAME"
+ssh -i ~/keypair.pem "ubuntu@$IP" "nohup /home/ubuntu/$BINARY_NAME $FLAGS &"
+
